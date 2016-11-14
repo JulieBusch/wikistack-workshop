4436b4fe9326a332205cc06b5b5577b6c648ea50
Julie Busch <heyjuliebusch@gmail.com>
2016-11-14 16:04:56 -0500

    finished section 5- showing a wiki page

--- routes/wiki.js
+++ routes/wiki.js
@@ -6,7 +6,10 @@ var Page = models.Page;
 var User = models.User;
 
 router.get('/', function(req, res, next){
-  res.redirect('/');
+  Page.findAll()
+  .then(function(pagesArray){
+    res.render('index', {pages: pagesArray});})
+  .catch(next);
 });
 
 router.post('/', function(req, res, next){
@@ -15,11 +18,24 @@ router.post('/', function(req, res, next){
     title: req.body.title,
     content: req.body.content
   });
-  res.json(page.save());
+  page.save().then(function(newPage){
+    res.redirect(newPage.route);
+  });
 });
 
 router.get('/add', function(req, res, next){
   res.render('addpage.html');
+});
+
+router.get('/:articleName', function(req, res, next){
+  //res.send('You did it!');
+  Page.findOne({
+    where: {
+      urlTitle: req.params.articleName
+    }})
+  .then(function(soughtPage){
+    res.render('wikipage', {page: soughtPage});})
+  .catch(next);
 });
 
 module.exports = router;
